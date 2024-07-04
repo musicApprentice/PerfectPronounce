d5178596cbdc5cc281937c22f290619b76fd30de
mgkandeshwarath <mkandeshwara@umass.edu>
2024-07-04 14:08:46 -0400

    made changes to all schemas

--- server/models/metricsSchema.js
+++ server/models/metricsSchema.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const metricsSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    card: {type: Number, required: true},
-    text:{type: String, required: true},
-    translation:{type: String, require: true},
-    audio:{type: String, require: true},
-    user: {type: mongoose.Schema.ObjectId, ref: "User" }
+    // Same as flashcards, have one collection with all grades from all classes and get grades per class as needed
+    classId: {type: mongoose.Schema.ObjectId, required: true},
+    assignment: {type: String, required: true},
+    card: {type: String, required: true},
+    email: {type: String, required: true},
+    timesPracticed: {type: Number, required: true},
+    score: {type: Number, required: true}
 })
 
 const Metrics = mongoose.model("assignment", metricsSchema);
