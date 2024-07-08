d117e6a4633173a309565b2f5a8eb567771896a2
Jason Huang <150495269+musicApprentice@users.noreply.github.com>
2024-07-07 20:56:39 -0400

    Revert "Quoc lam branch"

--- server/routes/assignments.js
+++ server/routes/assignments.js
@@ -1,26 +1,50 @@
-const {getAllAssignments, addNewAssignment, deleteAssignment,updateAssignment} = require('../database/assignmentFunctions');
 const express = require('express');
+const assignment = require('../models/assignmentSchema');
 const router = express.Router(); 
 
+router.get('/', async (req, res) => {
+    try {
+        const assignments = await assignment.find();
+        res.json(assignments);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+        console.log("assignment have no contents")
 
-// print out all the assignments in a particular class. 
-router.get('/', async(req, res) =>{
-    const assignments = await getAllAssignments(req.body.class_ID, req.body.assignment);
-    res.send(assignments)
+    }
 });
-//
 
-// Add assignment to class
 router.post('/', async (req, res) => {
-    await addNewAssignment(req,res);
+    const newAssign = new assignment({
+        name: req.body.name,
+        card: req.body.card,
+        text: req.body.text,
+        translation : req.body.translation,
+        audio : req.body.audio,
+    });
+    try {
+        const newAssignment = await newAssign.save();
+        res.status(201).json(newAssignment);
+        console.log("created new assignment")
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+        console.log("error created new assignment")
+    }
 });
- // delete all specific assignments in one class
- router.delete('/name/:name', async (req, res) => {
-    await deleteAssignment(req,res)
- });
 
- // update assignment in a particular class. This function requires 3 parameters: class_ID, name of assignment, and new assignment
-    router.put('/update/:update_assignment', async (req,res) =>{
-        await updateAssignment(req,res);
-    })
+router.delete('delete based on name and card', async (req, res) => {
+    try {
+        const nameNo = req.params.name;
+        const cardNo = req.params.card;
+        const delAssign = await assignment.findOne({$and:[{name: nameNo}, {card: cardNo}]});
+        if (delAssign === null) {
+            return res.status(404).json({ message: "Assignment not found" });
+        }
+        await assignment.deleteOne(delAssign);
+        res.json({ message: "Deleted Assignment" });
+    } catch (err) {
+        res.status(500).json({ message: `Internal server error: ${err.message}` });
+    }
+});
+
 module.exports = router; 
+
