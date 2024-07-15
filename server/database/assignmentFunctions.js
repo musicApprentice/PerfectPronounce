b5fecc7131135972ed194c51411fa4ebd2597c42
qlam2001 <90201171+qlam2001@users.noreply.github.com>
2024-07-14 23:14:27 -0400

    Update assignmentFunctions.js
    update the function addNewAssignment as requirements so now it works for an array of assignments. Also, I changed some variables' name and functions' name to make sure they are formal as possible.

--- server/database/assignmentFunctions.js
+++ server/database/assignmentFunctions.js
@@ -6,44 +6,37 @@ const FlashCard = require('../models/flashcard');
 var mongoose = require('mongoose');
 let db;
 
-async function connectToCoursesDB(){
-    await client.connect();
-    db = client.db("CoursesTest");
-}
-async function connectToUsersDB(){
+async function connectToDB(){
     await client.connect();
     db = client.db("test");
 }
-async function findAllStudentInClass(classID){
+async function findAllStudentInClass(classID,newStudent){
     const col = await db.collection("classes");
+    if(newStudent){
+        return [newStudent];
+    }
+    else{
     const temp = await col.find({"_id": classID}).toArray();
     const students = temp[0].students;
     return students;
+    }
 }
-// find email of student based on his/her id
-async function findEmail(idOfStudent){
-    await connectToUsersDB();
-    const col = await db.collection("users");
-    const temp = (await col.find({"_id": idOfStudent}).toArray());
-    const email = temp[0].email
-    return email;
-}
+
 // create empty metric for each flashcard for each student in a specific class
-async function createMetricsForStudent(assignmentName, classID, cardNo){
+async function createMetricsForStudent(assignmentName, classID, cardNo, newStudent){
     try{
-        await connectToCoursesDB();
-        const students = await findAllStudentInClass(classID)
-        const col = await db.collection("metrics")
+        await connectToDB();
+        const students = await findAllStudentInClass(classID, newStudent)
         students.forEach(async e => {
             const newMetric = new Metrics({
                 classId : classID,
                 assignment: assignmentName,
                 card: cardNo,
-                email: await findEmail(e),
+                email: e,
                 timesPracticed: 0,
                 score: 0
             })
-            await col.insertOne(newMetric)
+            await newMetric.save()
         })
     }
     catch(err){
@@ -52,68 +45,54 @@ async function createMetricsForStudent(assignmentName, classID, cardNo){
 }
 async function findAssignmentsByClassID(class_ID, assignmentName) {
     try {
-        await connectToCoursesDB();
+        await connectToDB();
         const query = assignmentName 
             ? { $and: [{"class_ID": class_ID}, {"assignment": assignmentName}] } 
             : { "class_ID": class_ID };
 
-        const assignments = await db.collection("flashcards").find(query).toArray();
-        return assignments;
+        var assignments = await db.collection("flashcards").find(query).toArray();
+        assignments = assignments.sort((a, b) => a.assignment.localeCompare(b.assignment));
+        return assignments
     } catch (err) {
         console.log("Cannot access the database");
     } 
 }
-async function addAssignmentToTheClass(class_ID, newAssignmentID,res){
-    await connectToCoursesDB();
+async function addAssignmentToTheClass(class_ID, newAssignmentID){
+    await connectToDB();
     const col = await db.collection("classes")
-    const temp = await col.find({"_id": class_ID}).toArray();
-    const assClaR = temp[0].assignments;
-    if(assClaR.some(e => e.equals(newAssignmentID))){
+    const tempClass = await col.find({"_id": class_ID}).toArray();
+    const arrayAssigment = tempClass[0].assignments;
+    if(arrayAssigment.some(e => e.equals(newAssignmentID))){
         console.log("Duplicate assignment")
     }
     else{
-        assClaR.push(newAssignmentID);
-        await col.updateOne({"_id": class_ID},{$set:{"assignments":assClaR}})
+        arrayAssigment.push(newAssignmentID);
+        await col.updateOne({"_id": class_ID},{$set:{"assignments":arrayAssigment}})
         console.log("Done for add assignment to the class")
     }
+} 
+async function createFlashcard(lengthOfAssignmentsInClass,col,classID,e){
+    const newAssignment = new FlashCard({
+        class_ID : classID,
+        assignment: e.assignment,
+        card: lengthOfAssignmentsInClass === 0 ? 1: lengthOfAssignmentsInClass + 1,
+        translation : e.translation,
+        audio: e.audio
+    })
+    await newAssignment.save();
+    const positionOfNewAssignment= await col.find({$and:[{"class_ID":classID},{"assignment": e.assignment},{"card":newAssignment.card}]}).toArray()
+    const newAssignment_ID = positionOfNewAssignment[0]._id;
+    await createMetricsForStudent(newAssignment.assignment,classID, newAssignment.card)
+    await addAssignmentToTheClass(classID, newAssignment_ID)
 }
 async function addNewAssignment(req, res){
     try {
         const classID = new mongoose.Types.ObjectId(req.body.class_ID);
-        const assignmentsInClass = (await findAssignmentsByClassID(classID, req.body.assignment)).length;
-        if(assignmentsInClass === 0){
-            const col = await db.collection("flashcards"); 
-            const newAssignment = new FlashCard({
-                class_ID : classID,
-                assignment: req.body.assignment,
-                card: 1,
-                translation : req.body.translation,
-                audio: req.body.audio
-            })
-            await col.insertOne(newAssignment);
-            const temp= await col.find({$and:[{"class_ID":classID},{"assignment": req.body.assignment},{"card": 1}]}).toArray()
-            const newAssignment_ID = temp[0]._id;
-            await createMetricsForStudent(newAssignment.assignment,classID, newAssignment.card)
-            await addAssignmentToTheClass(classID, newAssignment_ID,res)
-            res.send("created first one of the new assignment")
-        }
-         else{
-            const col = await db.collection("flashcards");
-            const newAssignment = new FlashCard({
-                class_ID : classID,
-                assignment: req.body.assignment,
-                card: assignmentsInClass + 1,
-                translation : req.body.translation,
-                audio: req.body.audio
-            })
-            await col.insertOne(newAssignment);
-            const temp= await col.find({$and:[{"class_ID":classID},{"assignment": req.body.assignment},{"card": assignmentsInClass + 1}]}).toArray()
-            const newAssignment_ID = temp[0]._id;
-            const prompt = `created assignment no. ${assignmentsInClass + 1} `
-            await createMetricsForStudent(newAssignment.assignment, classID, newAssignment.card)
-            await addAssignmentToTheClass(classID, newAssignment_ID,res)
-            res.send(prompt)
-        }
+        req.body.assignments.forEach(async e => {
+        const totalNumberOfThatAssignment = (await findAssignmentsByClassID(classID, e.assignment)).length;
+        const col = await db.collection("flashcards"); 
+        await createFlashcard(totalNumberOfThatAssignment,col,classID,e)
+        })
     } catch (err) {
         res.status(400).json({ message: err.message });
         console.log("error created new assignment no")
@@ -121,7 +100,7 @@ async function addNewAssignment(req, res){
 }
 // when you delete an assignment, the function will automatically delete the metrics of that assignment
 async function findIdOfRelevantFlashcard (class_ID, nameOfAssignment){
-    await connectToCoursesDB()
+    await connectToDB()
     const col = await db.collection("flashcards");
     const temp = await col.find({$and:[{"class_ID": class_ID},{"assignment": nameOfAssignment}]}).toArray();
     const arrOfFlashcard = temp.map(e => e._id)
@@ -129,7 +108,7 @@ async function findIdOfRelevantFlashcard (class_ID, nameOfAssignment){
 }
 async function deleteAssignment(req, res){
     try {
-        await connectToCoursesDB();
+        await connectToDB();
         const nameOfAssignment = req.body.assignment;
         const classID = new mongoose.Types.ObjectId(req.body.class_ID)
         const col = await db.collection("flashcards");
@@ -137,10 +116,10 @@ async function deleteAssignment(req, res){
         const col2 = await db.collection("classes")
         const arrOfAssign = await findAssignmentsByClassID(classID,nameOfAssignment)
         if(arrOfAssign.length !== 0){
-            const arrOfObID = await findIdOfRelevantFlashcard(classID, nameOfAssignment);
+            const arrOfOfID = await findIdOfRelevantFlashcard(classID, nameOfAssignment);
             const temp = await col2.find({"_id": classID}).toArray()
             let updateArray = temp[0].assignments;
-            arrOfObID.forEach(e => updateArray = updateArray.filter(e1 => !e1.equals(e)))
+            arrOfOfID.forEach(e => updateArray = updateArray.filter(e1 => !e1.equals(e)))
             await col.deleteMany({$and:[{"assignment" : nameOfAssignment, "class_ID" : classID}]});
             await col1.deleteMany({$and:[{"assignment" : nameOfAssignment, "classId" : classID}]});
             await col2.updateOne({"_id": classID},{$set:{"assignments": updateArray}})
@@ -156,7 +135,7 @@ async function deleteAssignment(req, res){
 //Likewise, when you update an assignment you have to update the metrics too.
 async function updateAssignment (req,res){
     try{
-        await connectToCoursesDB();
+        await connectToDB();
         const col = await db.collection("flashcards")
         const col1 = await db.collection("metrics");
         const classID = req.body.class_ID
@@ -175,4 +154,4 @@ async function updateAssignment (req,res){
     }
 
 }
-module.exports = {findAssignmentsByClassID,updateAssignment,deleteAssignment,addNewAssignment}
+module.exports = {findAssignmentsByClassID,updateAssignment,deleteAssignment,addNewAssignment,createMetricsForStudent}
