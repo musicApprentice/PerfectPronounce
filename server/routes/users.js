0d97273cc8225c562483eb1ce888952df33b6f77
qlam2001 <90201171+qlam2001@users.noreply.github.com>
2024-07-27 23:14:31 -0400

    Update users.js
    I moved every functions to separate file named "userFunctions.js". At there, I added two more features: hash password and verifyLogin. I did use bcrypt; therefore, if you want to run smoothly, you have to install bcrypt package via npm install bcrypt

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,51 +1,20 @@
+const {findUsers,createNewUser,deleteUser, verifyLogin} = require('../database/userFunctions')
 const express = require('express');
-const User = require('../models/userSchema');
 const router = express.Router(); 
 
 router.get('/', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-        console.log("users unsuccessfully sent")
-        //test
-
-    }
+    await findUsers(req,res)
 });
 
 router.post('/', async (req, res) => {
-    console.log(req.body)
-    const user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        school: req.body.selectedSchool,
-        role: req.body.role
-    });
-
-    try {
-        const newUser = await user.save();
-        res.status(201).json(newUser);
-        console.log("created new user")
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-        console.log("error creating new user")
-    }
+    await createNewUser(req,res)
 });
 
 router.delete('/email/:email', async (req, res) => {
-    try {
-        const email = req.params.email;
-        const user = await User.findOne({ email: email });
-        if (user === null) {
-            return res.status(404).json({ message: "User not found" });
-        }
-        await User.deleteOne({ email: email });
-        res.json({ message: "Deleted User" });
-    } catch (err) {
-        res.status(500).json({ message: `Internal server error: ${err.message}` });
-    }
+   await deleteUser(req,res)
 });
+router.get('/login', async(req,res,next) =>{
+    res.json(await verifyLogin(req,res,next))
+})
 
 module.exports = router; 
