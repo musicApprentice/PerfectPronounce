3e4f772ce0fec0a4ffc2645c8e6c628a37b85182
qlam2001 <90201171+qlam2001@users.noreply.github.com>
2024-07-27 23:23:18 -0400

    Create userFunctions.js
    1) remove all logics from the route to this file
    2) add feature hash password and verify login
    P/s: you should install bcrypt to run smoothly via npm install bcrypt

---
+++ server/database/userFunctions.js
@@ -0,0 +1,67 @@
+const User = require('../models/userSchema');
+const bcrypt = require('bcrypt')
+
+async function findUsers(req,res){
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+        console.log("users unsuccessfully sent")
+        //test
+    
+    }
+} 
+async function createNewUser(req,res){
+    const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        school: req.body.selectedSchool,
+        password: await hashPassWord(req.body.password),
+        role: req.body.role
+    });
+
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+        console.log("created new user")
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+        console.log("error creating new user")
+    }
+
+}
+async function deleteUser(req,res){
+    try {
+        const email = req.params.email;
+        const user = await User.findOne({ email: email });
+        if (user === null) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        await User.deleteOne({ email: email });
+        res.json({ message: "Deleted User" });
+    } catch (err) {
+        res.status(500).json({ message: `Internal server error: ${err.message}` });
+    }
+}
+async function hashPassWord(plainPass){
+    const salt = await bcrypt.genSalt()
+    const modifiedPass = await bcrypt.hash(plainPass,salt)
+    return modifiedPass
+}
+async function verifyLogin(req,res){
+    const email = req.body.email
+    const password = req.body.password
+    let confirm = false
+    await User.exists({"email":email}).exec().then(async e =>{
+         if(e){
+             const user = (await User.find({"_id":e}))[0]
+            await bcrypt.compare(password,user.password) ? confirm = true: confirm = false 
+         }
+     })
+     return confirm
+        
+}
+
+module.exports = {findUsers,createNewUser,deleteUser, verifyLogin}
