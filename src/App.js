7223726b18cb68d93f7a5a186741b75b920f2e1f
ShiyuZhang <shiyuzhang2003@gmail.com>
2024-07-29 01:49:40 -0400

    main page updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import logoSrc from './imgs/Language_Maestro.png'
 import UserList from "./components/universal/userList/UserList";
 import CreateUser from "./components/universal/createAccount/createUser";
 import Login from "./components/universal/login/login";
@@ -7,25 +8,42 @@ import AdminLayout from "./components/admin/adminLayout";
 import StudentLayout from "./components/student/studentLayout";
 import TeacherLayout from "./components/teacher/teacherLayout";
 
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+
+function AuthButton() {
+  const navigate = useNavigate();
+
+  const handleSignIn = () => {
+    navigate('/login');
+  };
+
+  const handleSignUp = () => {
+    navigate('/createAccount');
+  };
+
+  return (
+    <>
+      <button onClick={handleSignIn}>Sign in</button>
+      <button onClick={handleSignUp}>Sign up</button>
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <div className="App">
-
-        <nav>
-        <h1> Language Maestro</h1>
-
-          <ul>
-            <h2>
-              <Link to="/createAccount">Create Account</Link>
-            </h2>
-            <h2>
-              <Link to="/login">Login to Account</Link>
-            </h2>
-          </ul>
-        </nav>
+      <div class="background-container"></div>
+      <div class="overlay">
+        <div class="header">
+            <h1><img src = {logoSrc} className="logo" alt="logo"></img></h1>
+            <div className="auth-buttons">
+              <AuthButton />
+            </div>
+        </div>
+        <div class="content">
+            <h2>Experience a new era of foreign language learning</h2>
+            <button class="get-started" onclick="getStarted()">Get Started</button>
+        </div>
 
         <Routes>
           <Route path="/createAccount" element={<CreateUser />} />
